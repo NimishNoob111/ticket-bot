ab4d09b08637f5aa3b9970fa053f2f521336f61c
NimishNoob111 <nimishhandsome69@gmail.com>
2026-07-12 14:46:52 +0530

    models directory

--- models/dashboardModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema ({
-    guildID: String,
-    url: String,
-    port: String,
-});
-
-module.exports = mongoose.model('dashboard', schema);
