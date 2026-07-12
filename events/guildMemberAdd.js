bae7104d40010cb02c1cad033594622acfe5fc8e
NimishNoob111 <nimishhandsome69@gmail.com>
2026-07-12 14:46:42 +0530

    events directory

--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-const fs = require('fs');
-const yaml = require("js-yaml")
-const config = yaml.load(fs.readFileSync('./config.yml', 'utf8'))
-
-module.exports = async (client, member) => {
-
-};
